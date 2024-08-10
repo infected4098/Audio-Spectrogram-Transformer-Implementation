0973efde7f8de68d5391392c8f5d65130b399f2d
Yong Joon Lee <112607019+infected4098@users.noreply.github.com>
2024-08-10 23:44:34 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,3 +13,30 @@ def envelope(y, rate, threshold):
 
 def normalize_mel(S, min_level_db = -100):
     return np.clip((S-min_level_db)/-min_level_db, 0,1)
+
+
+def summarize_model(model, input_shape, is_cuda = False):
+  global device
+
+  if is_cuda:
+    x = torch.rand(input_shape).to(device)
+  else:
+    x = torch.rand(input_shape)
+  print(parameter_count_table(model))
+
+  print(summary(model, input_size = input_shape))
+
+
+def test_model(model, X_test, y_test, device):
+  model.eval()
+  inputs = X_test
+  labels = y_test
+
+  with torch.no_grad():
+      inputs = torch.FloatTensor(inputs).to(device)  # (#data, #dim, #timesteps)
+      labels = torch.FloatTensor(labels).to(device)  # (#data, #labels)
+      utputs = model(inputs)
+      preds = torch.argmax(outputs, dim=1)
+      labels = torch.argmax(labels)
+  return np.array(preds.cpu()), np.array(labels.cpu())
+
